b9833d6625c87543f16a73acd882d895cb6e1fa0
arthurbrandao0 <arthurbm15@gmail.com>
2022-12-16 10:48:10 -0300

    adding Webdriver Manager

--- src/selenium_java_test/TestOne.java
+++ src/selenium_java_test/TestOne.java
@@ -3,6 +3,7 @@ package selenium_java_test;
 import org.openqa.selenium.By;      
 import org.openqa.selenium.WebDriver;              
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
  
 public class TestOne {
  
@@ -10,31 +11,25 @@ public class TestOne {
        
         System.setProperty("webdriver.chrome.driver", "F:/Selenium/chromedriver.exe");
        
-        WebDriver driver= new ChromeDriver();          
+        WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();         
  
-        //Maximize window      
-        driver.manage().window().maximize();    
-       
-        //Launch the Site.      
         driver.get("https://www.lambdatest.com/selenium-playground/simple-form-demo");
        
-        //Enter values into form and submit      
-        driver.findElement(By.id("sum1")).sendKeys("2");                    
-        driver.findElement(By.id("sum2")).sendKeys("25");    
+        driver.findElement(By.id("sum1")).sendKeys("5");                    
+        driver.findElement(By.id("sum2")).sendKeys("5");    
         driver.findElement(By.xpath("//*[@id='gettotal']/button")).click();  
        
-        //Verify the result
         String result = driver.findElement(By.xpath("//*[@id=\'addmessage\']")).getText();
         
         System.out.println(result);
-        if( Integer.parseInt(result) == 7) {
+        if( Integer.parseInt(result) == 10) {
              System.out.println("Test Success");             
 
         } else {
              System.out.println("Test Failed");
         }
-        //Close the browser
-        Thread.sleep(4000);
+        
         driver.quit();              
     }
 }
